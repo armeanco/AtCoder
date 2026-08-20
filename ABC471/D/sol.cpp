f8eda971b5be0c1241d2fec35b21051c57db1dac
Mnatsakanyan <104078312+armeanco@users.noreply.github.com>
2026-08-20 23:14:56 +0400

    Create sol.cpp

---
+++ ABC471/D/sol.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int q, v, x;
+    std::cin >> q >> v;
+    int i = 0;
+    std::set<std::vector<int>> what;
+    while(q--) {
+        int a, b;
+        std::cin >> x;
+        if(x == 1) {
+            std::cin >> a >> b;
+            what.insert({a >= b ? (b - a) : (b - a), a, b});
+            i++;
+        }
+        if(x == 2) {
+            std::cin >> b;
+            if(what.empty()) std::cout << -1 << '\n';
+            else {
+                auto ti = *what.rbegin();
+                std::cout << (((b - ti[1]) + ti[2]) >= v ? v : ((b - ti[1]) + ti[2])) << "\n";
+                if(!what.empty()) what.erase(std::next(what.rbegin()).base());
+            }
+            i++;
+        }
+    }
+    return 0;
+}
